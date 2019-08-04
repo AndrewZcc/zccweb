f062e66ffac851eb118402bd9d823710013d565c
zccMac <racekey123@qq.com>
2019-08-04 16:52:38 +0800

    func: import second-level labels.

--- models/notes_model.py
+++ models/notes_model.py
@@ -9,14 +9,14 @@ class NoteCategory(db.Model):
     __tablename__ = 'note_category'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(30), nullable=False)
-    order = db.Column(db.Integer, default=1)
+    order = db.Column(db.Integer, nullable=False, default=1)
 
 
 class Note(db.Model):
     __tablename__ = 'note'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(300), nullable=False)
-    create_time = db.Column(db.DateTime, default=datetime.now)
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
     content_path = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref=db.backref('notes'))
